dd806bd2561f8ccbe65626dc1c23d76b8148b147
Joshua <joshuatay.tay@gmail.com>
2023-06-19 21:28:45 +0800

    added global theme colors

--- src/pages/articles/index.page.tsx
+++ src/pages/articles/index.page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Article } from "../../../types";
+import article from "../dummyJSON/article.json";
 
 const ArticlesPage: React.FC = () => {
-  return <div>ArticlesPage</div>;
+  const placeholderArticle: Article = article[0];
+  const [articles, setArticles] = useState<Article[]>([placeholderArticle]);
+
+  return (
+    <div className="flex justify-center min-h-screen min-w-full">
+      <div>ArticlesPage</div>
+    </div>
+  );
 };
 
 export default ArticlesPage;
