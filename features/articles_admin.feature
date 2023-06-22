4e59d21e4a498ab472efc0fe1d655a0d7af7a0ea
Lim Yu_Jie <lim.yujie@gmail.com>
2023-06-22 15:36:29 +0800

    doing automated testing

--- features/articles_admin.feature
+++ features/articles_admin.feature
@@ -7,7 +7,7 @@ Feature: Admin Dashboard - Articles
     Scenario: Viewing 'Articles' tab but no articles were added
         Given an admin lands in the admin dashboard for the first time
         And is in the 'Chat' tab
-        When the admin hovers over the 'Articles' button
+        When the admin hovers over the 'Articles' tab
         And clicks on the 'View Articles' button
         Then the admin should see the text 'You have no articles, please create a new article!'
     
@@ -15,7 +15,7 @@ Feature: Admin Dashboard - Articles
     Scenario: Viewing 'Articles' tab with articles added
         Given an admin lands in the admin dashboard for the first time
         And is in the 'Chat' tab
-        When the admin hovers over the 'Articles' button
+        When the admin hovers over the 'Articles' tab
         And clicks on the 'View Articles' button
         Then the admin should see a list of all the articles that all admins have added to the page
     
@@ -28,7 +28,7 @@ Feature: Admin Dashboard - Articles
     #4
     Scenario: Accessing the page to create articles
         Given an admin is in the 'Chat' tab of the admin dashboard
-        When the admin hovers over the 'Articles' button
+        When the admin hovers over the 'Articles' tab
         And clicks on the 'Create Article' button
         # for following step, for puppeteer check the page-title
         Then the admin should see a form to create articles
--- features/step_definitions/adminArticle.js
+++ features/step_definitions/adminArticle.js
@@ -15,7 +15,7 @@ After(async () => {
   await browser.close();
 });
 
-// Scenario: Viewing articles but no articles were added
+// #1 Scenario: Viewing articles but no articles were added
 Given('an admin lands in the admin dashboard for the first time', async () => {
   await page.goto('http://localhost:3000');
 });
@@ -38,44 +38,72 @@ Then('the admin should see the text \'You have no articles, please create a new 
   expect(messageText).to.equal('You have no articles, please create a new article!');
 });
 
-// Scenario: Viewing 'Articles' tab with articles added
+// #2 Scenario: Viewing 'Articles' tab with articles added
 Given('an admin lands in the admin dashboard for the first time', async () => {
-    await page.goto('http://localhost:3000');
-  });
+  await page.goto('http://localhost:3000');
+});
 
-  Given('is in the \'Chat\' tab', async () => {
-    await page.click('[data-testid="chat-tab"]');
-  });
+Given('is in the \'Chat\' tab', async () => {
+  await page.click('[data-testid="chat-tab"]');
+});
 
-  When('the admin hovers over the \'Articles\' button', async () => {
-    await page.hover('[data-testid="articles-tab"]');
-  });
+When('the admin hovers over the \'Articles\' button', async () => {
+  await page.hover('[data-testid="articles-tab"]');
+});
 
-  When('clicks on the \'View Articles\' button', async () => {
-    await page.click('[data-testid="viewarticles-tab"]');
-  });
+When('clicks on the \'View Articles\' button', async () => {
+  await page.click('[data-testid="viewarticles-tab"]');
+});
 
-  Then('the admin should see a list of all the articles that all admins have added to the page', async () => {
-    const articles = await page.$$('.article');
-    expect(articles.length).to.be.above(0);
-  });
+Then('the admin should see a list of all the articles that all admins have added to the page', async () => {
+  const articles = await page.$$('.article');
+  expect(articles.length).to.be.above(0);
+});
 
-// Scenario: Opening one of the articles listed in the 'Articles' tab
+
+// #3 Scenario: Opening one of the articles 
 Given('an admin is in the \'View Articles\' page of the admin dashboard', async () => {
     await page.goto('http://localhost:3000/articles');
     originalTabsCount = (await browser.pages()).length;
-  });
+});
 
-  When('the admin clicks on the \'Visit Link\' button', async () => {
-    const button = await page.$('[data-testid="visit-article-button"]');
-    expect(button).to.not.be.null;
-    await button.click();
-  });
+When('the admin clicks on the \'Visit Link\' button', async () => {
+  const button = await page.$('[data-testid="visit-article-button"]');
+  expect(button).to.not.be.null;
+  await button.click();
+});
 
-  Then('the admin is redirected to the article\'s original URL in a new browser tab', async () => {
-    const newTabsCount = (await browser.pages()).length;
-    expect(newTabsCount).to.be.equal(originalTabsCount + 1);
-    const newPage = (await browser.pages())[newTabsCount - 1];
-    const newPageUrl = await newPage.url();
-    expect(newPageUrl).to.include('https://');
-  });
+Then('the admin is redirected to the article\'s original URL in a new browser tab', async () => {
+  const newTabsCount = (await browser.pages()).length;
+  expect(newTabsCount).to.be.equal(originalTabsCount + 1);
+  const newPage = (await browser.pages())[newTabsCount - 1];
+  const newPageUrl = await newPage.url();
+  expect(newPageUrl).to.include('https://');
+});
+
+// #4 Scenario: Accessing the page to create articles
+Given('an admin is in the \'Chat\' tab of the admin dashboard', async () => {
+  await page.goto('http://localhost:3000/chat');
+});
+
+When('the admin hovers over the \'Articles\' tab', async () => {
+  const buttonSelector = '[data-testid="articles-tab"]';
+  await this.page.waitForSelector(buttonSelector);
+  await this.page.hover(buttonSelector);
+});
+
+When('clicks on the \'Create Article\' button', async () => {
+  const createArticleButtonSelector = '[data-testid="visit-article-button"]';
+  await Promise.all([
+    this.page.waitForNavigation(),
+    this.page.click(createArticleButtonSelector),
+  ]);
+});
+
+
+Then('the user is in the create article page', async () => {
+  const expectedUrl = 'http://localhost:3000/articles/new';
+  const currentUrl = await this.page.url();
+  expect(currentUrl).to.equal(expectedUrl);
+});
+
