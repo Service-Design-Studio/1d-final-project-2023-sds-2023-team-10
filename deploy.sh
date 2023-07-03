514cb1b0efe5163cf2fe45c015cd6ce33bdbb29f
vinroger <vincentiusrkuswara@gmail.com>
2023-07-03 22:06:41 +0800

    migrate project

--- deploy.sh
+++ deploy.sh
@@ -14,16 +14,16 @@ exec 3>&1
 
 {
   echo -e "\n\033[1;33mBuilding the Docker image...\033[0m" >&3
-  docker build -t gcr.io/rubyintro/admin-dashboard . --no-cache && echo -e "\n\033[1;32mDocker image built successfully!\033[0m" >&3
+  docker build -t gcr.io/echolestia/admindashboard . --no-cache && echo -e "\n\033[1;32mDocker image built successfully!\033[0m" >&3
 
   echo -e "\n\033[1;33mAuthenticating Docker to gcloud...\033[0m" >&3
   gcloud auth configure-docker && echo -e "\n\033[1;32mDocker authenticated to gcloud successfully!\033[0m" >&3
 
   echo -e "\n\033[1;33mPushing the Docker image to gcloud...\033[0m" >&3
-  docker push gcr.io/rubyintro/admin-dashboard && echo -e "\n\033[1;32mDocker image pushed successfully!\033[0m" >&3
+  docker push gcr.io/echolestia/admindashboard && echo -e "\n\033[1;32mDocker image pushed successfully!\033[0m" >&3
 
   echo -e "\n\033[1;33mDeploying the Docker image to Cloud Run...\033[0m" >&3
-  gcloud run deploy admin-dashboard --image gcr.io/rubyintro/admin-dashboard --platform managed --region asia-southeast1 --allow-unauthenticated && echo -e "\n\033[1;32mDocker image deployed to Cloud Run successfully!\033[0m" >&3
+  gcloud run deploy admindashboard --image gcr.io/echolestia/admindashboard --platform managed --region asia-southeast1 --allow-unauthenticated && echo -e "\n\033[1;32mDocker image deployed to Cloud Run successfully!\033[0m" >&3
 } &
 
 while kill -0 $! >/dev/null 2>&1; do
--- src/pages/api/chatroom.page.ts
+++ src/pages/api/chatroom.page.ts
@@ -1,8 +1,8 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs';
-import path from 'path';
+import { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
 
-export const data = require('../dummyJSON/chatRooms.json');
+export const data = require("../dummyJSON/chatroom.json");
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   // const filePath = path.resolve('../', 'dummyJSON', 'chatRooms.json');
