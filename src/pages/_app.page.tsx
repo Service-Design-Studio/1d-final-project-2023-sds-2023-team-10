fe80e169c652325f4f06e69aec397e47b6681c3a
vinroger <vincentiusrkuswara@gmail.com>
2023-07-04 22:36:02 +0800

    fix bug

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
         },
       }}
     >
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ConfigProvider>
   );
 }
