d28efce3968eb841ac63edaf6772ca00d1880a5a
Lim Yu_Jie <94518915+lim-yujie@users.noreply.github.com>
2023-07-27 21:21:36 +0800

    updated user chatbot feature file added user chatbot puppeteer files

--- features/step_definition/Chatbot/Scenario_1.js
+++ features/step_definition/Chatbot/Scenario_1.js
@@ -4,15 +4,20 @@ const puppeteer = require('puppeteer');
 
 let browser, page;
 
-Given('a user is on the chatbot page', async function () {
-  browser = await puppeteer.launch({headless:false});
-  page = await browser.newPage();
-  await page.goto('http://localhost:3000/login',{ waitUntil: 'networkidle0', timeout: 60000 });  // replace with your login/signup page url
-  await page.type('#email', '1');
-  await page.type('#password', '2');
-  const loginButtonSelector = '[data-testid="login-button"]'; // replace with your button selector
-  await Promise.all([
-      page.click(loginButtonSelector), // Triggers navigation
-      page.waitForNavigation({ waitUntil: 'networkidle0' })  // Waits until navigation finishes
-  ]);
+Given('a user is on the messages tab', async function () {
+    browser = await puppeteer.launch({headless:false});
+    page = await browser.newPage();
+    // placeholder for code
+  });
+  
+When("the user opens a chat", {timeout: 60 * 1000}, async function () {
+    // placeholder for code
+});
+
+When("clicks on the 'Chatbot' button on the top right", {timeout: 60 * 1000}, async function () {
+    // placeholder for code
+});
+
+Then("the user will reach the page where they can talk to the chatbot", {timeout: 60 * 1000}, async function () {
+    // placeholder for code
 });
--- features/user_chatbot.feature
+++ features/user_chatbot.feature
@@ -14,4 +14,4 @@ Scenario: Entering the chatbot and sending a message
     And types 'I am so stressed out'
     And clicks the arrow button to send message
     And sees the message appear in the chat
-    Then the user recieves a reply from the chatbot
+    Then the user receives a reply from the chatbot
