2441bab1179d693b65bec69d4cea741260ba1e77
vinroger <vincentiusrkuswara@gmail.com>
2023-08-06 17:54:21 +0800

    styling improvement on article

--- src/pages/articles/index.page.tsx
+++ src/pages/articles/index.page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-nested-ternary */
 import React, { useEffect, useState } from "react";
-import { Button, Card, Popconfirm, Spin, message } from "antd";
+import { Alert, Button, Card, Popconfirm, Spin, message } from "antd";
 import { DeleteOutlined } from "@ant-design/icons";
 import Meta from "antd/lib/card/Meta";
 import Image from "next/image";
@@ -91,63 +91,73 @@ const ArticlesPage: React.FC = () => {
             <img
               alt={article.title}
               src={article.img_url}
-              className="w-full h-auto mx-auto object-cover"
+              className="w-full h-36 mx-auto object-cover rounded-lg"
             />
           </div>
         }
         loading={isLoading}
-        extra={
-          <Popconfirm
-            title="Are you sure you want to delete this article?"
-            onConfirm={() => deleteArticle(article.id)}
-            okText={<span data-testid={`yes-button-${article.id}`}>Yes</span>}
-            cancelText={<span data-testid={`no-button-${article.id}`}>No</span>}
-          >
-            <div
-              className="flex items-center justify-center w-8 h-8 bg-red-300 text-white rounded-full"
-              data-testid={`delete-article-button-${article.id}`}
-            >
-              <DeleteOutlined />
-            </div>
-          </Popconfirm>
-        }
+        bodyStyle={{ height: "400px", paddingTop: "5px" }}
       >
-        <Meta />
-        <h1 className="text-gray-600 mt-2">Title: {article.title}</h1>
-        <h3 className="text-gray-600 mt-2">Author: {article.author}</h3>
-        <p className="text-gray-600 mt-2">
-          Published Date:{" "}
-          {article.published_date &&
-            article.published_date.toISOString().substring(0, 10)}
-        </p>
-        <p className="text-gray-600">
-          Created Date:{" "}
-          {article.created_date &&
-            article.created_date.toISOString().substring(0, 10)}
-        </p>
-        <Button
-          type="primary"
-          data-testid="visit-article-button"
-          onClick={() => handleUrlButton(article.url)}
-        >
-          Visit Link
-        </Button>
-        <p className="text-gray-600">
-          User Group: {article.user_group.join(", ")}
-        </p>
+        <div className="flex flex-col content-start min-h-full">
+          <h4 className="text-gray-600 mt-2">Title: {article.title}</h4>
+          <p className="text-gray-600 mt-2">Author: {article.author}</p>
+          <p className="text-gray-600 mt-2">
+            Published Date:{" "}
+            {article.published_date &&
+              article.published_date.toISOString().substring(0, 10)}
+          </p>
+          <p className="text-gray-600">
+            Created Date:{" "}
+            {article.created_date &&
+              article.created_date.toISOString().substring(0, 10)}
+          </p>
+
+          <p className="text-gray-600">
+            User Group: {article.user_group.join(", ")}
+          </p>
+          <div className="flex flex-row min-w-full justify-between mt-auto">
+            {" "}
+            <Button
+              type="primary"
+              data-testid="visit-article-button"
+              onClick={() => handleUrlButton(article.url)}
+            >
+              Visit Link
+            </Button>{" "}
+            <Popconfirm
+              title="Are you sure you want to delete this article?"
+              onConfirm={() => deleteArticle(article.id)}
+              okText={<span data-testid={`yes-button-${article.id}`}>Yes</span>}
+              cancelText={
+                <span data-testid={`no-button-${article.id}`}>No</span>
+              }
+            >
+              <div
+                className="flex items-center justify-center w-8 h-8 bg-red-400 hover:bg-red-300 cursor-pointer text-white rounded-full"
+                data-testid={`delete-article-button-${article.id}`}
+              >
+                <DeleteOutlined />
+              </div>
+            </Popconfirm>
+          </div>
+        </div>
       </Card>
     ));
   };
 
   return (
     <MainLayout>
+      <Alert
+        description="These are the articles that will be shown to the users. Click articles and create articles to add more articles."
+        type="info"
+        showIcon
+        className="h-18 mt-3"
+      />
       <div className="flex-col justify-center min-h-screen min-w-full">
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 p-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4 p-4">
           {isLoading ? (
-            <div className="flex justify-center align-middle">
-              <div>
-                <h1>Loading...</h1>
-              </div>
+            <div className="flex justify-center align-middle font-normal">
+              <div className="font-light text-sm">Loading...</div>
             </div>
           ) : articles.length === 0 ? (
             <div className="flex justify-center align-middle">
